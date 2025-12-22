7c9bd63a0d5dac8b253f04162f47eeb532f58ab0
leanmarqs <leanmarqs@gmail.com>
2025-12-22 13:19:43 -0300

    feat: add ESLint and Prettier configuration, update dependencies, and enhance toast notifications in the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { Button } from "@/components/ui/button"
-
-import { toast } from "sonner"
+import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
 
 function App() {
+  const handleClick = () => {
+    const toastId = toast.loading('Salvando alterações...')
+
+    // Simula uma ação assíncrona (ex: request)
+    setTimeout(() => {
+      toast.success('Salvo com sucesso', {
+        description: 'As alterações foram aplicadas.',
+        id: toastId, // substitui o loading pelo success
+      })
+    }, 3000)
+  }
+
   return (
-    <>
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button
-          onClick={() =>
-            toast.success("Salvo com sucesso", {
-              description: "As alterações foram aplicadas.",
-            })
-          }
-        >
-          Click me
-        </Button>
-      </div>
-    </>
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <Button onClick={handleClick}>Click me</Button>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from '@/components/ui/sonner'
 import App from './App.tsx'
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,9 +13,8 @@ createRoot(document.getElementById('root')!).render(
       richColors
       toastOptions={{
         duration: 2000,
-        className:
-          "bg-background border border-border shadow-lg rounded-xl",
-        descriptionClassName: "text-muted-foreground",
+        className: 'bg-background border border-border shadow-lg rounded-xl',
+        descriptionClassName: 'text-muted-foreground',
       }}
     />
   </StrictMode>,
